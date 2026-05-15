dc0983aca93532ef7d82b11371b3f0854b7ad384
ShreeHari <hinali.lukhi@gmail.com>
2026-05-15 15:33:44 +0200

    setting up the manifest file

---
+++ custom_addons/aircraft_maintenance/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    "name": "Aircraft Maintenance",
+    "version": "1.0",
+    "category": "Operations",
+    "summary": "Track aircraft components, usage, and maintenance cycles",
+    "author": "Your Name",
+    "depends": ["base"],
+    "data": [],
+    "installable": True,
+    "application": True,
+}
+
